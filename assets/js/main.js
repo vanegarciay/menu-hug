528f9049b1d3f46e96f501d5ae6f923aed1056fb
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-14 22:47:49 -0400

    menu finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,16 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
     var currentScroll = window.pageYOffset || document.body.scrollTop;
     if(currentScroll > lastScrollTop){
-        console.log("down");
+        document.getElementById("header").classList.remove("header-in");
+        document.getElementById("header").style.opacity = 0;
     } else{
-        console.log("up");
+        document.getElementById("header").classList.add("header-in");
+        document.getElementById("header").classList.add("solid");
+        document.getElementById("header").style.opacity = 1;
+        if(currentScroll<=3){
+            document.getElementById("header").classList.remove("header-in");
+            document.getElementById("header").classList.remove("solid");
+        }
     }
     lastScrollTop = currentScroll;
-});
+}, false);
